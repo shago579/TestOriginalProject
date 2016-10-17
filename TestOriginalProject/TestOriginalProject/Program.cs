698d781250764f34b1fd59ff92dd784ce9757683
Santiago <santiago@ordico.mx>
2016-10-17 11:47:21 -0500

    Calculo de area de un rectangulo

--- TestOriginalProject/TestOriginalProject/Program.cs
+++ TestOriginalProject/TestOriginalProject/Program.cs
@@ -12,9 +12,10 @@ namespace TestOriginalProject
         static void Main(string[] args)
         {
             var cuadrado = new Square(4.5m);
+            var rectangulo = new Rectangle(4.5m, 2m);
 
             System.Console.WriteLine("Un cuadrado con un lado de 4.5 cm tiene de area: " + cuadrado.Area);
-            
+            System.Console.WriteLine("Un rectangulo con una base de 4.5 cm y una altura de 2 cm tiene de area: " + rectangulo.Area);
         }
     }
 }
